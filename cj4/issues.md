57429f655268d572a4641a7e9d3b41b92b15e3b9
Marius Krämer <flieger-marius@web.de>
2021-03-20 13:18:35 +0100

    testing cj4 template

---
+++ cj4/issues.md
@@ -0,0 +1,15 @@
+---
+layout: cj4
+title: Known Issues
+coverimage: masterwarn.jpg
+---
+
+# Working Title CJ4
+
+## UPDATES
+Latest Version: [v0.11.3](https://github.com/Working-Title-MSFS-Mods/fspackages/releases/tag/cj4-v0.11.3)
+
+## KEY FEATURES
+
+*   Issue 1
+*   Issue 2
---
+++ docs/js/script.js
@@ -0,0 +1,63 @@
+
+function customscroll(type, element = false) {
+    if (type == "content-top") {
+        element = document.getElementsByClassName("whitesection")[0];
+    }
+    const y = element.getBoundingClientRect().top + window.pageYOffset - 200;
+
+    window.scrollTo({ top: y, behavior: 'smooth' });
+    return false;
+}
+
+function isElementInViewport(element) {
+    let rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        let topbar = document.getElementsByClassName("topbar")[0];
+        let menubar = document.getElementsByClassName("menubar")[0];
+        let toggle = document.getElementsByClassName("menutoggle")[0];
+        let menu = document.getElementsByClassName("menu")[0];
+
+        if (window.location.hash) {
+            customscroll("element", document.getElementById(window.location.hash.substr(1)));
+        };
+
+        toggle.addEventListener('click', function (e) {
+            menu.classList.toggle("active");
+            topbar.classList.toggle("active");
+            menubar.classList.toggle("active");
+        });
+
+        let h2s = document.getElementsByTagName("h2");
+        Array.from(h2s).forEach(function (el) {
+            if (isElementInViewport(el)) {
+                el.classList.add("in");
+            } else {
+                el.classList.remove("in");
+            };
+        })
+
+        document.addEventListener('scroll', function (e) {
+            if (window.scrollY != 0) {
+                topbar.classList.add("dark");
+                menubar.classList.add("dark");
+            } else {
+                topbar.classList.remove("dark");
+                menubar.classList.remove("dark");
+            };
+            Array.from(h2s).forEach(function (el) {
+                if (isElementInViewport(el)) {
+                    el.classList.add("in");
+                } else {
+                    el.classList.remove("in");
+                };
+            });
+        });
+    };
+};
